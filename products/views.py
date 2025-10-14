29e84bbaed66b0904a3f1f4657a0de786c07533a
Maksim Tyrin <makstyrin@mail.ru>
2025-10-14 16:49:28 +0800

    Добавил поиск

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Product, Category, Favorite
 
 
@@ -12,11 +13,18 @@ class ProductListView(ListView):
     context_object_name = 'products'
 
     def get_queryset(self):
-        return Product.objects.prefetch_related('images').order_by('?')
+        queryset = Product.objects.prefetch_related('images')
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset.order_by('?')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['current_category'] = None
+        context['search_query'] = self.request.GET.get('q', '')
         if self.request.user.is_authenticated:
             context['user_favorites'] = set(
                 self.request.user.favorites.values_list('product_id', flat=True)
@@ -33,11 +41,18 @@ class ProductListByCategoryView(ListView):
 
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
-        return Product.objects.filter(category=self.category).prefetch_related('images').order_by('?')
+        queryset = Product.objects.filter(category=self.category).prefetch_related('images')
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        return queryset.order_by('?')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['current_category'] = self.category
+        context['search_query'] = self.request.GET.get('q', '')
         if self.request.user.is_authenticated:
             context['user_favorites'] = set(
                 self.request.user.favorites.values_list('product_id', flat=True)
