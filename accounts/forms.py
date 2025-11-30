1b7482e6d74472447a1249a45e5293268fef0092
Maksim Tyrin <makstyrin@mail.ru>
2025-11-30 16:21:50 +0800

    Добавил email

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,13 +4,28 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+    email = forms.EmailField(required=False, label='Email')
     class Meta:
         model = Profile
-        fields = ['first_name', 'last_name', 'address', 'phone', 'avatar']
+        fields = ['first_name', 'last_name', 'address', 'phone', 'avatar', 'email']
         widgets = {
             'address': forms.Textarea(attrs={'rows': 3}),
             'phone': forms.TextInput(attrs={'placeholder': '+7 (999) 123-45-67',}),
         }
+
+    def save(self, commit=True):
+        profile = super().save(commit=False)
+    
+        user = profile.user
+        user.first_name = self.cleaned_data.get('first_name', '')
+        user.last_name = self.cleaned_data.get('last_name', '')
+        user.email = self.cleaned_data.get('email', '')
+        
+        if commit:
+            user.save()
+            profile.save()
+        
+        return profile
 
 class CustomPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.CharField('Имя', max_length=100, blank=True)
     last_name = models.CharField('Фамилия', max_length=100, blank=True)
+    email = models.EmailField(blank=True)
     address = models.TextField('Адрес', blank=True)
     phone = models.CharField('Телефон', max_length=16, blank=True)
     avatar = models.ImageField('Аватар', upload_to='avatars/', blank=True, null=True)
